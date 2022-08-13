d550c38e34ee7b1ef3b7e659ff154617a4f7a73a
unknown <vinayakvkp97@gmail.com>
2022-08-13 16:34:19 +0530

    still collecting signatures not good enough

--- collecting-signatures.py
+++ collecting-signatures.py
@@ -4,19 +4,23 @@ def contain(a,b):
         return True
     else:
         return False
-def index(l,a):
-    i = 0
-    j = 0
-    for i in range(len(l)):
+def index(l,a,k,n):
+    i = k-1
+
+    j = k
+    while(i<n):
         if(contain(a,l[i])):
             j+=1
+            
+            
+        i+=1
     return j
 
-print("enter number of line segments")
+#print("enter number of line segments")
 nsegments = int(input())
 
 l =[]
-print("please enter the coordinates of the line segments you wish to enter")
+#print("please enter the coordinates of the line segments you wish to enter")
 for i in range(nsegments):
    
     l.append(list(map(int,input().split(' '))))
@@ -24,16 +28,23 @@ for i in range(nsegments):
    
 
 l.sort(key=lambda x:x[1])
-print(l)
+#print(l)
 ans = []
-i = 0
+i = 1
 temp = 0
+
+
 while(i<nsegments):
-    ans.append(l[i][1])
-    temp = l[i][1]
-    i = index(l,temp)
-print(ans)
+    ans.append(l[i-1][1])
+   
+    temp = l[i-1][1]
+    i = index(l,temp,i-1,nsegments)
+    i+=1
+    
+    
 print(len(ans))
+print(*ans,sep = ' ')
+
    
 
     
